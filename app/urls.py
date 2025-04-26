e2b56124a6871405a12421cafbee88718b6eeb3c
saa81 <saadatakmatova122@gmail.com>
2025-04-26 16:04:09 +0300

    add css

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
-    path('hello/<str:name>/', views.greet, name='greet')
+    path('hello/<str:name>/', views.greet, name='greet'),
+    path('boooks/', views.book_list, name='books'),
+    path('index/', views.my_page, name='my_page'),
+    path('pt-05/', views.pt, name='pt')
 ]
--- app/views.py
+++ app/views.py
@@ -13,10 +13,17 @@ def book_list(request):
     books_text = ', '.join([books.title for book in books])
     return HttpResponse(f"Ваши книги {books_text}")
 
+def my_page(request):
+    return render(request, template_name='index.html')
+
+def pt(request):
+    return render(request, template_name='pt-05.html')
+
+
+
 def how_are_you(request):
     return HttpResponse("Как дела?")
 
 def good_bye(request):
     return HttpResponse("Пока!")
 
-# Create your views here.
